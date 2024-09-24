f2b476d066e9693d43a6248ac45401893e0e8899
monke <ramensoda228@gmail.com>
2024-09-24 23:26:09 +0500

    fix route

--- index.php
+++ index.php
@@ -7,11 +7,14 @@ if($_SERVER['REQUEST_URI'] == '/'){
 
 $token = file_get_contents("token.txt");
 
+
 if($_SERVER['HTTP_AUTHORIZATION'] != "Bearer ".$token){
     echo "Ошибка: Не авторизован";
     header("HTTP/1.1 401 Unauthorized");
     die();
 }
 
+header('HTTP/1.1 307 Temporary Redirect');
 header("Location: /server.php".$_SERVER['REQUEST_URI']);
+exit;
 ?>
